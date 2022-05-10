8124707a351e24fe5e3396b937a6b6c576623594
Tungthanh97 <tung020197@gmail.com>
2022-05-10 23:49:34 +0700

    add patients

---
+++ src/pages/PatientDetail/PatientDetail.tsx
@@ -0,0 +1,19 @@
+/* Libs */
+import React from 'react';
+import { Grid } from '@mui/material';
+
+const PatientDetail: React.FC = () => {
+
+
+    return (
+        <Grid container spacing={4}>
+            <Grid item xs={false} md={12}>
+                <Grid >
+
+                </Grid>
+            </Grid>
+        </Grid>
+    )
+}
+
+export default PatientDetail;
